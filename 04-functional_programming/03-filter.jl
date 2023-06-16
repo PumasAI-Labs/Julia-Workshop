44bdf5fcf052d5dcd7b98c078f3992c54aabcba6
jotas6 <j.oneto@pumas.ai>
2023-06-16 16:15:43 -0500

    Updated code example for the `filter` function

--- 04-functional_programming/03-filter.jl
+++ 04-functional_programming/03-filter.jl
@@ -7,12 +7,23 @@ filter(iseven, x) # Get all even numbers (iseven(x) = true)
 filter(!iseven, x) # Get all odd numbers (negate iseven)
 filter(isodd, x) # Get all odd numbers (isodd(x) = true)
 
-## Custom function: find names that contain the letter "e" and have at least 5 characters
-is_interesting(name) = contains(name, "e") && (length(name) ≥ 5) # The return value should be a Boolean variable
+## Common use case: remove missing values
+obs = [
+    missing,
+    0.067,
+    110,
+    220,
+    220,
+    missing,
+    110,
+    58,
+    missing,
+    76
+]
 
-names = ["Alice", "Juan", "Bob", "Jake"]
-filter(is_interesting, names) # Jake is too short
+filter(i -> i != missing, obs) # Doesn't work, we need to use the ismissing function
+filter(!ismissing, obs) # Negate ismissing to get all the values that are not missing
 
 # Count occurrences that satisfy a certain condition
 count(iseven, x) # There are 5 even numbers between 1 and 10
-count(is_interesting, names) # There is only one interesting name in our list
+count(ismissing, obs) # Count how many missing observations are there
