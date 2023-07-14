8f5dea35779e4c0da8ad635c5e67466e6115f485
Juan Jose Gonzalez Oneto <j.oneto@pumas.ai>
2023-07-14 20:49:38 +0000

    Updated code examples for `reduce` and `mapreduce`

--- 04-functional_programming/02-reduce.jl
+++ 04-functional_programming/02-reduce.jl
@@ -11,12 +11,29 @@ sum(x)
 prod(x)
 
 ### Custom function
-square_sum(accumulated, x) = accumulated + x^2
-reduce(square_sum, x; init=0) # Need to set the accumulated value to 0 at the start
-sum([i^2 for i in x]) # Check
+times = [0, 1, 2, 4, 8, 12, 24]
+obs = [0.01, 112, 224, 220, 143, 109, 57]
 
-### Tip: use reduce and map
-reduce(+, map(i -> i^2, x))
+"""
+Simple function to calculate the AUC  with reduce
+using the trapezoidal rule (https://en.wikipedia.org/wiki/Trapezoidal_rule)
+"""
+function AUC(accumulated, i)
+    
+    trapz_area = (obs[i] + obs[i+1])*(times[i+1] - times[i]) / 2
+
+    return accumulated + trapz_area
+
+end
+
+reduce(AUC, 1:length(obs)-1; init=0) # Need to set the accumulated value to 0 at the start
+
+## Common use case: reduce + map
+using Statistics
+reduce(+, map(i -> (i - mean(x))^2, x)) / (length(x) - 1) # Sample variance of x
+
+### Tip: You can calculate the sample variance var from Statistics
+var(x)
 
 ## mapreduce -> map + reduce, but better
-mapreduce(i -> i^2, +, x) # mapreduce(<function for map>, <function for reduce>, <collection>)
+mapreduce(i -> (i - mean(x))^2, +, x) / (length(x) - 1) # mapreduce(<map>, <reduce>, <collection>)
